106b2ef94d5380b07726fdfdcfc1930eb75c0c2a
Pranali1299 <pranalipatil3620@gmail.com>
2021-02-22 18:40:29 +0530

    Calculator file with add function Added

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Calculator {
+	
+	public static void add(int a, int b){
+		
+		System.out.print(a+b);
+	}
+
+
+	public static void main(String[] args){
+	
+		Calculator obj = new Calculator();
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		obj.add(a,b);
+		
+	}
+}
